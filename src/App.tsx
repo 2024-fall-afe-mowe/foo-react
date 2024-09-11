0f60a5a4cfedafbfa735638dcedea2f8b8d4d47d
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-09-11 15:10:13 -0500

    mapped a list of names
    listed out names

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const players = ["Caden J", "Tom S", "Charles L", "Enzo F"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Players</h1>
+      <ul>
+        {players.map((x) => (
+          <li>{x}</li>
+        ))}
+      </ul>
     </div>
   );
 }
